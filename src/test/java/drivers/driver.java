47c0849e09dcb3ba25233033f446d35a98f24866
Erica Hagle <erica.hagle@me.com>
2017-08-11 13:50:56 -0400

    Remove AfterClass/BeforeClass imports from driver class

--- src/test/java/drivers/driver.java
+++ src/test/java/drivers/driver.java
@@ -4,9 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
 
 import java.util.HashMap;
